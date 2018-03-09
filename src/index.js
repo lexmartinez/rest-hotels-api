e7731790c0d633486644c3770206d69182bf8bf9
Lex Martinez <lex0316@gmail.com>
2018-03-08 23:05:44 -0500

    add compression - cache express middleware

--- src/index.js
+++ src/index.js
@@ -7,15 +7,20 @@ import favicon from 'serve-favicon';
 import routes from './routes';
 import logger from 'morgan';
 import cors from 'cors';
+import compression from 'compression';
+import apicache from 'apicache';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8080;
+const cache = apicache.middleware;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+app.use(compression());
+app.use(cache());
 app.use(logger('dev'));
 app.use(express.static('public'));
 app.use('/v1', routes);
